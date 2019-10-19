292b472f5c28db06b0b6054dc40851c1f35646ff
Kevin Lin <kevin.lin.2@stonybrook.edu>
2019-10-19 11:47:36 -0400

    hi

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const cors = require('cors');
 const path = require('path');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors);
 
 app.get('/', (req, res, next) => {
    try {
