cd77aa615cda33b95ebb484bb30525b81c27bab8
Kevin Lin <kevin.lin.2@stonybrook.edu>
2019-10-19 12:39:48 -0400

    backend done

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 const express = require('express');
 const app = express();
+const OpenTok = require('opentok');
+const { apiKey, projectSecret } = require('./secrets');
 const axios = require('axios');
 const path = require('path');
+let opentok = new OpenTok(apiKey, projectSecret);
+let setTime = new Date().getTime();
+let sessionId = '';
+let token = '';
+
+const createSession = () => {
+   opentok.createSession({ mediaMode: 'routed' }, (err, session) => {
+      if (err) {
+         console.log("couldn't create session");
+      } else {
+         sessionId = session.sessionId;
+      }
+   });
+};
+const createToken = () => {
+   token = session.generateToken({
+      expireTime: new Date().getTime() / 1000 + 7 * 24 * 60 * 60, // in one week
+   });
+};
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res, next) => {
-   try {
-      // console.log(path.join(__dirname, 'public', 'index.html'));
-      res.sendFile(path.join(__dirname, '../public/index.html'));
-   } catch (error) {
-      res.send(error.message);
+app.use(() => {
+   if (!sessionId) {
+      createSession();
    }
+   if (!token.length || new Date().getTime() - setTime <= 0) {
+      createToken();
+      setTime = new Date().getTime() / 1000 + 7 * 24 * 60 * 60;
+   }
+   opentok.startArchive();
+});
+app.get('/', (req, res, next) => {
+   res.sendFile(path.join(__dirname, '../public/index.html'));
+   res.send(error.message);
 });
 app.get('/lol', (req, res, next) => {
    res.send('it works');
--- public/js/app.js
+++
@@ -1,7 +0,0 @@
-// replace these values with those generated in your TokBox Account
-const apiKey = 'YOUR_API_KEY';
-const sessionId = 'YOUR_SESSION_ID';
-const token = 'YOUR_TOKEN';
-
-// (optional) add server code here
-// initializeSession();
