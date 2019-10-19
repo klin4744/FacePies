f0250e7b817dbd2e3baf31fc1b1fb12aaa06cc0a
Kevin Lin <kevin.lin.2@stonybrook.edu>
2019-10-19 18:09:41 -0400

    last commit

--- app.js
+++ app.js
@@ -27,24 +27,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(() => {
-//    if (!sessionId) {
-//       createSession();
-//    }
-//    if (!token.length || new Date().getTime() - setTime <= 0) {
-//       createToken();
-//       setTime = new Date().getTime() / 1000 + 7 * 24 * 60 * 60;
-//    }
-//    opentok.startArchive();
-// });
-app.get('/', (req, res, next) => {
-   res.sendFile(path.join(__dirname, '../public/index.html'));
-   res.send(error.message);
-});
-app.get('/lol', (req, res, next) => {
-   res.send('it works');
-});
-
 const PORT = 8000 || process.env.port;
 app.listen(PORT, (req, res, next) => {
    console.log('Server is running on port ' + PORT);
--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,5 @@
 // replace these values with those generated in your TokBox Account
-const apiKey = '46441642';
-const sessionId =
-   '1_MX40NjQ0MTY0Mn5-MTU3MTUwMDMxMDM5OX5jOWpscmwwelZLMWlKZk5BdjJ3dEU3V3R-fg';
-const token =
-   'T1==cGFydG5lcl9pZD00NjQ0MTY0MiZzaWc9NjUxNGI3MDI0MGE4YzI4ZDhiOGVhY2VhZWUyYjk0NjVlOGE4MDhlZjpzZXNzaW9uX2lkPTFfTVg0ME5qUTBNVFkwTW41LU1UVTNNVFV3TURNeE1ETTVPWDVqT1dwc2Ntd3dlbFpMTVdsS1prNUJkakozZEVVM1YzUi1mZyZjcmVhdGVfdGltZT0xNTcxNTAxMDUzJm5vbmNlPTAuMzkyMzgzMjI4MzM0NTg2OTQmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU3MTU4NzQ1MiZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==';
+const { token, sessionId, apiKey } = require('../../secrets');
 // (optional) add server code here
 initializeSession();
 // Handling all of our errors here by alerting them
